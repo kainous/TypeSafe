5f8a8b88ed5165fcd9999b8582a26e0c50221e86
Nicholas Cooper <kainous@hotmail.com>
2019-02-15 14:20:05 -0600

    Working through minimum logic

---
+++ Paraconsistency/MinimumLogic.fs
@@ -0,0 +1,32 @@
+﻿module MinimumLogic
+
+type Minimum =
+| Falsity
+| Imply of (Minimum * Minimum)
+
+let (-->) a b = Imply(a, b)
+let ( ! ) p = p --> Falsity
+
+let ( && ) p q = !(p --> !q)
+let ( || ) p q = (p --> q) --> q
+let ( <--> ) p q = (p --> q) && (q --> p)
+
+let (|AND|) p q =
+  match p, q with
+  | Imply(p, Imply(q, False))
+
+let evalIPC = function
+| Imply(p, Imply(q, r))
+
+// Schema 1 introduces q out of nowhere
+| Imply(p, Imply(q, r)) -> (p --> q) --> (p --> r) // Schema 2
+| Imply(p, Imply(_, r)) when p = r -> p // Pierce's Law - Schema 3
+
+let evalMinimal = function
+| Imply(Falsity, Imply(Falsity, b)) -> Imply(Falsity, b)
+
+let evalIntuitionistic = function
+| Imply(Falsity, a) -> a
+
+let evalClassical = function
+| Imply(Imply(a, Falsity), Falsity) -> a
---
+++ Paraconsistency/Program.fs
@@ -0,0 +1,25 @@
+﻿module ParaconsistentLogic
+type Paraconsistent =
+| Truth
+| Pro  of string
+| Neg' of Paraconsistent
+| Con' of Paraconsistent * Paraconsistent
+| Eq   of Paraconsistent * Paraconsistent
+| Eq'  of Paraconsistent * Paraconsistent
+
+let Falsity  = Neg' Truth
+let Dis' p q = Neg'(Con'((Neg' p), (Neg' q)))
+let Imp p q  = Eq(p, Con'(p, q))
+let Imp' p q = Eq'(p, Con'(p, q))
+let Box p = Eq(p, Truth)
+let Neg p = Box(Neg' p)
+let Con p q = Box(Con'(p, q))
+let Dis p q = Box(Dis' p  q)
+
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // return an integer exit code
+
+
